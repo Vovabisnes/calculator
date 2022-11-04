d5624feee74c66240a73c2359877d98f94fbf7b5
Wlady <Volodymyr.Havryliuk@quinscape.de>
2022-11-04 13:30:56 +0100

    Main program is ready

---
+++ src/Calculator.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Calculator {
+    private double firstNumber;
+    private double secondNumber;
+    private String operator;
+    private static final ArrayList<String> operators = new ArrayList<>();
+
+    static {
+        operators.add("+");
+        operators.add("-");
+        operators.add("/");
+        operators.add("*");
+    }
+    public void dataInput(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter an operator: ");
+        while (true){
+            operator = scanner.next();
+            if (operators.contains(operator)) {
+                break;
+            } else {
+                System.out.println("Enter the right operator");
+            }
+        }
+
+        System.out.println("Enter the first number: ");
+        while (true) {
+            String line = scanner.next();
+            try {
+                firstNumber = Double.parseDouble(line);
+                break;
+            } catch (Exception e) {
+                System.out.println("Enter the right number: ");
+            }
+        }
+
+        System.out.println("Enter the second number: ");
+        while (true) {
+            String line = scanner.next();
+            if (line.equals("0")) {
+                System.out.println("Division by 0 is not possible");
+                System.out.println("Enter the right number: ");
+                continue;
+            }
+            try {
+                secondNumber = Double.parseDouble(line);
+                break;
+            } catch (Exception e) {
+                System.out.println("Enter the right number: ");
+            }
+        }
+    }
+
+    public double calculate() {
+        if (operator.equals(operators.get(0))) {
+            return firstNumber + secondNumber;
+        } else if (operator.equals(operators.get(1))) {
+            return firstNumber - secondNumber;
+        } else if (operator.equals(operators.get(2))) {
+            return firstNumber / secondNumber;
+        } else if (operator.equals(operators.get(3))) {
+            return firstNumber * secondNumber;
+        }
+        return 0;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double result;
+        Calculator calculator = new Calculator();
+        calculator.dataInput();
+        result = calculator.calculate();
+        System.out.println("Your result is: " + result);
     }
 }
