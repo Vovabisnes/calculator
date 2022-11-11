421636d573500bee66c76123d838fe1d965b426e
Wlady <Volodymyr.Havryliuk@quinscape.de>
2022-11-11 12:18:41 +0100

    Hauptprogramm ist fertig

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,71 +1,25 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Calculator {
-    private double firstNumber;
-    private double secondNumber;
-    private String operator;
-    private static final ArrayList<String> operators = new ArrayList<>();
-
-    static {
-        operators.add("+");
-        operators.add("-");
-        operators.add("/");
-        operators.add("*");
-    }
-    public void dataInput(){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter an operator: ");
-        while (true){
-            operator = scanner.next();
-            if (operators.contains(operator)) {
-                break;
-            } else {
-                System.out.println("Enter the right operator");
-            }
-        }
-
-        System.out.println("Enter the first number: ");
-        while (true) {
-            String line = scanner.next();
-            try {
-                firstNumber = Double.parseDouble(line);
-                break;
-            } catch (Exception e) {
-                System.out.println("Enter the right number: ");
-            }
-        }
-
-        System.out.println("Enter the second number: ");
-        while (true) {
-            String line = scanner.next();
-            if (line.equals("0")) {
-                System.out.println("Division by 0 is not possible");
-                System.out.println("Enter the right number: ");
-                continue;
-            }
-            try {
-                secondNumber = Double.parseDouble(line);
-                break;
-            } catch (Exception e) {
-                System.out.println("Enter the right number: ");
-            }
-        }
-    }
-
+    private final Reader reader = new Reader();
     public double calculate() {
-        if (operator.equals(operators.get(0))) {
-            return firstNumber + secondNumber;
-        } else if (operator.equals(operators.get(1))) {
-            return firstNumber - secondNumber;
-        } else if (operator.equals(operators.get(2))) {
-            return firstNumber / secondNumber;
-        } else if (operator.equals(operators.get(3))) {
-            return firstNumber * secondNumber;
+        switch (reader.getOperator()) {
+            case "+":
+                return reader.getFirstNumber() + reader.getSecondNumber();
+            case "-":
+                return reader.getFirstNumber() - reader.getSecondNumber();
+            case "*":
+                return reader.getFirstNumber() * reader.getSecondNumber();
+            case "/":
+                return reader.getFirstNumber() / reader.getSecondNumber();
         }
         return 0;
     }
 
-
+    public void printTheResult(double number){
+        String num = String.valueOf(number);
+        if (num.charAt(num.length()-2)=='.' && num.charAt(num.length()-1)=='0'){
+            System.out.printf("Your result is: %.0f", number);
+        } else {
+            System.out.println("Your result is: " + number);
+        }
+    }
 }
---
+++ src/Reader.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class Reader {
+    private double firstNumber;
+    private double secondNumber;
+    private String operator;
+    private final Scanner scanner = new Scanner(System.in);
+
+    public Reader() {
+        readData();
+    }
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    private void readData(){
+        System.out.println("Enter an operator: ");
+        operator = readOperator();
+
+        System.out.println("Enter the first number: ");
+        firstNumber = readNumber();
+
+        System.out.println("Enter the second number: ");
+        while (true){
+            secondNumber = readNumber();
+            if (secondNumber==0 && operator.equals("/")){
+                System.out.println("Division by 0 is impossible");
+                System.out.println("Enter the right number");
+            } else {
+                break;
+            }
+        }
+    }
+
+    private String readOperator(){
+        String operatorValue;
+        while (true){
+            operatorValue = scanner.next();
+            if (operatorValue.equals("+") || operatorValue.equals("-") || operatorValue.equals("/") || operatorValue.equals("*")) {
+                return operatorValue;
+            } else {
+                System.out.println("Enter the right operator");
+            }
+        }
+    }
+
+    private double readNumber(){
+        while (true) {
+            String line = scanner.next();
+            double number;
+            try {
+                number =  Double.parseDouble(line);
+                return number;
+            } catch (Exception e) {
+                System.out.println("Enter the right number: ");
+            }
+        }
+    }
+}
